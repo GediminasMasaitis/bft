a8eb2892566fdbd182ab88a5963d4670db9bf14e
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2026-01-19 22:14:17 +0200

    Update optimizer.c

--- src/optimizer.c
+++ src/optimizer.c
@@ -69,6 +69,63 @@ void create_zeroing_sets(Program *output, const Program *input) {
   program_calculate_loops(output);
 }
 
+void optimize_memset(Program *output, const Program *input) {
+  memset(output, 0, sizeof(*output));
+
+  addr_t out_index = 0;
+  for (addr_t in_index = 0; in_index < input->size; in_index++) {
+    const Instruction instr = input->instructions[in_index];
+
+    if (instr.op == OP_SET) {
+      i32 in_set_val = instr.arg;
+      i32 count = 1;
+
+      /* Count consecutive SET value, RIGHT 1 patterns */
+      addr_t j = in_index;
+      while (j + 2 < input->size) {
+        const Instruction *right = &input->instructions[j + 1];
+        const Instruction *next_set = &input->instructions[j + 2];
+
+        /* Check for RIGHT 1 followed by SET with same value */
+        if (right->op == OP_RIGHT && right->arg == 1 &&
+            next_set->op == OP_SET && next_set->arg == in_set_val) {
+          count++;
+          j += 2; /* Move past RIGHT and SET */
+        } else {
+          break;
+        }
+      }
+
+      if (count >= 2) {
+        /* Emit the memset SET with count */
+        output->instructions[out_index].op = OP_SET;
+        output->instructions[out_index].arg = in_set_val;
+        output->instructions[out_index].arg2 = count;
+        out_index++;
+
+        /* Emit the pointer movement: we set 'count' cells, so move count-1 to
+         * end at last cell */
+        output->instructions[out_index].op = OP_RIGHT;
+        output->instructions[out_index].arg = count - 1;
+        out_index++;
+
+        /* Skip past all the instructions we consumed */
+        in_index = j;
+        continue;
+      }
+    }
+
+    /* Default: copy instruction (ensure arg2 is set for SET) */
+    output->instructions[out_index] = instr;
+    if (instr.op == OP_SET && output->instructions[out_index].arg2 == 0) {
+      output->instructions[out_index].arg2 = 1;
+    }
+    out_index++;
+  }
+  output->size = out_index;
+  program_calculate_loops(output);
+}
+
 void optimize_seek_empty(Program *output, const Program *input) {
   memset(output, 0, sizeof(*output));
 
@@ -511,8 +568,8 @@ void optimize_program(Program *program) {
     create_zeroing_sets(&optimized, program);
     *program = optimized;
 
-    // optimize_memset(&optimized, program);
-    // *program = optimized;
+    optimize_memset(&optimized, program);
+    *program = optimized;
 
     optimize_seek_empty(&optimized, program);
     *program = optimized;
