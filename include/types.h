5c0bf67d87b5bdd2ef77864e6c62799fc4ca4334
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2026-01-31 13:41:52 +0200

    Prepare unions for refactor

--- include/types.h
+++ include/types.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <stdint.h>
 
 typedef char i8;
@@ -17,9 +16,10 @@ typedef enum {
   OP_LOOP = '[',
   OP_END = ']',
   OP_DONE = '\0',
-
-  // Extended
-
+  
+  // IR opcodes
+  OP_MOVE = OP_RIGHT,
+  OP_ADD = OP_INC,
   OP_SET = '=',
   OP_SEEK_EMPTY = 'E',
   OP_TRANSFER = 'T',
@@ -55,11 +55,80 @@ typedef struct {
 
 typedef struct {
   op_t op;
-  i32 arg;
-  i32 arg2;
-  i32 offset;
-  i32 stride;
-  TransferTarget targets[MAX_TRANSFER_TARGETS];
+  union {
+    struct {
+      i32 arg;
+      i32 arg2;
+      i32 offset;
+      i32 stride;
+      TransferTarget targets[MAX_TRANSFER_TARGETS];
+    };
+
+    struct {
+      i32 distance;
+    } right;
+
+    struct {
+      i32 amount;
+      i32 _inc_pad;
+      i32 offset;
+    } inc;
+
+    struct {
+      i32 value;
+      i32 count;
+      i32 offset;
+      i32 stride;
+    } set;
+
+    struct {
+      i32 step;
+      i32 _seek_pad;
+      i32 offset;
+    } seek;
+
+    struct {
+      i32 target_count;
+      i32 is_assignment;
+      i32 src_offset;
+      i32 _transfer_pad;
+      TransferTarget targets[MAX_TRANSFER_TARGETS];
+    } transfer;
+
+    struct {
+      i32 match_addr;
+      i32 _loop_pad;
+      i32 offset;
+    } loop;
+    
+    struct {
+      i32 divisor;
+      i32 _div_pad;
+      i32 src_offset;
+      i32 _div_pad2;
+      TransferTarget targets[MAX_TRANSFER_TARGETS];
+    } div;
+
+    struct {
+      i32 divisor;
+      i32 _mod_pad;
+      i32 src_offset;
+      i32 _mod_pad2;
+      TransferTarget targets[MAX_TRANSFER_TARGETS];
+    } mod;
+
+    struct {
+      i32 _in_pad1;
+      i32 _in_pad2;
+      i32 offset;
+    } in;
+
+    struct {
+      i32 _out_pad1;
+      i32 _out_pad2;
+      i32 offset;
+    } out;
+  };
 } Instruction;
 
 typedef struct {
